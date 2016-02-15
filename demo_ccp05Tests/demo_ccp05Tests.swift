5ea974514b7d758f92ef86d5feca2ee0a29d7ba4
chanthicha <FaiFai@faifais-macbook-pro.local>
2016-02-16 05:22:54 +0700

    Initial Commit

---
+++ demo_ccp05Tests/demo_ccp05Tests.swift
@@ -0,0 +1,36 @@
+//
+//  demo_ccp05Tests.swift
+//  demo_ccp05Tests
+//
+//  Created by FaiFai on 2/16/2559 BE.
+//  Copyright © 2559 FaiFai. All rights reserved.
+//
+
+import XCTest
+@testable import demo_ccp05
+
+class demo_ccp05Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
